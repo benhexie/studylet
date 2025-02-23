e8f1e8273f125c670e521fde40a4bcb0eeac4a42
benhexie <benedictgabriel73@gmail.com>
2025-02-23 13:22:17 +0100

    Cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin, Access-Control-Allow-Credentials');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   
   // Handle preflight requests
