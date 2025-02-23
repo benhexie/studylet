df25813a60e32ba49ef761d552ceb6258cde4fa1
benhexie <benedictgabriel73@gmail.com>
2025-02-23 13:52:36 +0100

    Cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -17,17 +17,7 @@ dotenv.config({ path: `${__dirname}/../.env` });
 const app = express();
 
 // Configure CORS to allow all origins
-// @ts-ignore
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
-  
-  if (req.method === 'OPTIONS') {
-    return res.status(204).end();
-  }
-  next();
-});
+app.use(cors());
 
 // Regular middleware
 app.use(express.json({ limit: "50mb" }));
